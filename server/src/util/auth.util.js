ffd6671749bb756338216402efbd00a68fb6f69b
DangaRanga <jasonblue1626@gmail.com>
2021-03-13 18:37:16 -0500

    Completed fronted for login/signup

---
+++ server/src/util/auth.util.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (request, response, next) => {
+  try {
+    const token = request.header("x-access-token");
+    if (!token) {
+      return response
+        .status(401)
+        .json({ msg: "No authentication token, access denied" });
+    }
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) {
+      return response
+        .status(401)
+        .json({ msg: "Token verification failed, authorization denied" });
+    }
+    request.user = verified.id;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
